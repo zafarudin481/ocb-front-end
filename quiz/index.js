b560d4141415ca1a5c0c32305f36c325746f0466
zafarudin481 <zafarudin.jaffir@gmail.com>
2024-12-03 13:27:45 +0800

    finished quiz card skeleton

--- quiz/index.js
+++ quiz/index.js
@@ -1,52 +1,7 @@
-const quizList = [
-    {
-        question: "Ketua negara Malaysia ialah _____",
-        answerOption: ["Yang di Pertuan Agong", "Raja", "Menteri Besar"],
-        answerIndex: 0
-    },
-    {
-        question: "Apakah bunga kebangsaan Malaysia?",
-        answerOption: ["Bunga Ros", "Bunga Kertas", "Bunga Raya"],
-        answerIndex: 2
-    },
-    {
-        question: "Kesopanan dan kesusilaan merupakan prinsip Rukun Negara yang ke berapa?",
-        answerOption: ["Pertama", "Kelima", "Keempat"],
-        answerIndex: 1
-    },
-    {
-        question: "Apakah Bahasa Kebangsaan Malaysia?",
-        answerOption: ["Bahasa Melayu", "Bahasa Inggeris", "Bahasa Mandarin"],
-        answerIndex: 0
-    },
-    {
-        question: "Bilakah tarikh Kemerdekaan Malaysia?",
-        answerOption: ["31 Ogos 1963", "16 September 1963", "31 Ogos 1957"],
-        answerIndex: 2
-    },
-    {
-        question: "Siapakah Bapa Kemerdekaan negara kita?",
-        answerOption: ["Tunku Abdul Rahman", "Tun Hussein Onn", "Tun Dr. Mahathir Mohamad"],
-        answerIndex: 0
-    },
-    {
-        question: "Apakah nama lain bagi Bendera Malaysia?",
-        answerOption: ["Jalur Malaysia", "Jalur Gemilang", "Jalur Bendera"],
-        answerIndex: 1
-    },
-    {
-        question: "Negeri di bawah bayu merujuk kepada negeri _____",
-        answerOption: ["Selangor", "Sabah", "Sarawak"],
-        answerIndex: 1
-    },
-    {
-        question: "Berapakah prinsip dalam Rukun Negara?",
-        answerOption: ["6 Prinsip", "3 Prinsip", "5 Prinsip"],
-        answerIndex: 2
-    },
-    {
-        question: "Siapakah Perdana Menteri Malaysia yang ke-10?",
-        answerOption: ["Datuk Seri Anwar Ibrahim", "Datuk Seri Najib Tun Razak", "Tun Dr. Mahathir Mohamad"],
-        answerIndex: 0
-    }
-]
+// event listener to initiate the quiz
+
+const initiateButton = document.getElementById("initiate-button");
+
+const eventToAlert = initiateButton.addEventListener("click", function () {
+    alert("butang mula sudah ditekan");
+})
