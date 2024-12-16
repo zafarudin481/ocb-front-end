2f8a7eb2f4db20ddcd109eb7b8482d23bb0fc4d5
zafarudin481 <zafarudin.jaffir@gmail.com>
2024-12-16 10:18:20 +0800

    add automatic load more photos at home page

--- search-gallery/home-photos.js
+++ search-gallery/home-photos.js
@@ -1,26 +1,47 @@
 var pexelsKey = config.PEXELS_KEY
 
-let homeImages = document.getElementById('photos-container')
+const homeImages = document.getElementById('photos-container')
+const loadMore = document.getElementById('load-more')
+let nextPageURL
 
-fetch('https://api.pexels.com/v1/curated', {
-    headers: {
-        'Authorization': `${pexelsKey}`
-    }
-})
-    .then(response => response.json())
-    .then(body => {
-        const photosList = body.photos.map((images, index) => body.photos[index])
-        return photosList
-    })
-    .then(photosList => {
-        photosList.forEach((image) => {
-            updateHomePhotos(image)
-        });
-    })
-    .catch(err => { debugger })
+fetchPhotos()
 
-// function to insert list of photos into home
+// function to fetch and display photos
+function fetchPhotos(url = 'https://api.pexels.com/v1/curated') {
+    fetch(url, {
+        headers: {
+            'Authorization': `${pexelsKey}`
+        }
+    })
+        .then(response => response.json())
+        .then(body => {
+            const photosList = body.photos.map((images, index) => body.photos[index])
+            nextPageURL = body.next_page
+            return photosList
+        })
+        .then(photosList => {
+            photosList.forEach((image) => {
+                updateHomePhotos(image)
+            });
+            onVisible(loadMore, () => fetchPhotos(nextPageURL));
+        })
+        .catch(err => { debugger })
+}
+
+// function to insert list of photos into photos container
 function updateHomePhotos(image) {
-    homeImages.innerHTML += `<img class='col' src="${image.src.medium}" alt="${image.src.alt}">`
-    console.log("success")
+    homeImages.innerHTML += `<img class='col' src="${image.src.portrait}" alt="${image.alt}">`
+}
+
+// function to trigger load more photos
+function onVisible(element, callback) {
+    new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+                callback(element);
+                observer.disconnect();
+            }
+        });
+    }).observe(element);
+    if (!callback) return new Promise(r => callback = r);
 }
